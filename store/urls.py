e2803edc2a127b5107a4b7bb0f6735901028d02a
ardaatahan <63500745+ardaatahan@users.noreply.github.com>
2020-08-14 11:26:48 +0300

    add authentication

--- store/urls.py
+++ store/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('', views.store, name='store'),
     path('cart/', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 
     # API routes
     path('update_item/', views.update_item, name='update_item'),
--- store/views.py
+++ store/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib.auth.models import User
 
 from datetime import datetime
 import json
@@ -8,6 +12,58 @@ from .models import *
 from .utils import *
 
 # Create your views here.
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("store"))
+        else:
+            return render(request, "store/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "store/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("store"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "store/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "store/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("store"))
+    else:
+        return render(request, "store/register.html")
+
+
 def store(request):
 
     # Get the customer, order, and items depending on the user authentication
